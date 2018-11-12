9c9f185c0171650edf5e8af1ce72af2d2fae35a1
Rodolfo Perez <rodolfobeste@gmail.com>
2018-11-12 13:33:39 +0200

    Created functions for displaying selected students and creating pagination

--- js/script.js
+++ js/script.js
@@ -1,41 +1,18 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+
+const page_container = document.querySelector(".page");
+const student_item = document.querySelectorAll(".student-item");
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+const showPage = (list, start_list, end_list) => {
 
+  // Loop for hidding all students
+  const hideAll = list.forEach(student => student.style.display = "none");
 
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-
+  // Loop for displaying selected students
+  for ( let i = start_list; i <= end_list; i += 1 ) {
+    list[i].style.display = "block"
+  }
+}
 
 
 /*** 
@@ -43,8 +20,29 @@ FSJS project 2 - List Filter and Pagination
    functionality to the pagination buttons.
 ***/
 
+// Function for adding total pages
 
+const appendPageLinks = total_students => {
 
+  // for obatining the amount of pages to create depending on amount of students
+  let number_of_pages = Math.floor(total_students.length / 10);
 
+  // UL to create and its content
+  let ul = document.createElement("UL");
+  let pages = "";
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+  //Loop for creating pages as list items
+  for (let i = 0; i <= number_of_pages; i += 1) {
+    
+    pages += `
+    <li>
+      <a href="#">${i + 1}</a>
+    </li>
+    `
+  }
+
+  //Output pages
+  ul.innerHTML = pages;
+  page_container.appendChild(ul);
+
+}
